888b5ace083fef13d346523ce6ca97688407ea8f
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-01-29 23:11:02 +0700

    Database Config

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// SetupDatabaseConnection is creating a new connection to our database
+func SetupDatabaseConnection() *gorm.DB {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	// we will define models here
+	// db.AutoMigrate()
+	return db
+}
+
+// CloseDatabaseConnection is closing the database connection
+func CloseDatabaseConnection(db *gorm.DB) {
+	dbSQL, err := db.DB()
+	if err != nil {
+		panic("Failed to close database connection")
+	}
+
+	dbSQL.Close()
+}
