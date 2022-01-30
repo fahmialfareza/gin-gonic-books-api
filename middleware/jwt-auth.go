1466295c274384a98349bfe7a83047061861ce9e
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-01-30 09:30:45 +0700

    JWT Auth Middleware

---
+++ middleware/jwt-auth.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/fahmialfareza/go_gonic_api/helper"
+	"github.com/fahmialfareza/go_gonic_api/service"
+	"github.com/gin-gonic/gin"
+)
+
+// AuthorizeJWT is a middleware for JWT
+func AuthorizeJWT(jwtServ service.JWTService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Authorization")
+		if authHeader == "" {
+			response := helper.BuildErrorResponse("Failed to process request", "No token found", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		token, err := jwtServ.ValidateToken(authHeader)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			log.Println("Claim[user_id]: ", claims["user_id"])
+			log.Println("Claim[issuer]: ", claims["issuer"])
+		} else {
+			log.Println(err)
+			response := helper.BuildErrorResponse("Token is not valid", err.Error(), nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+	}
+}
