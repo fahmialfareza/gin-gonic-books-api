6c384edf2f94a48a5aba041232587d8c4dc3f922
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-01-30 19:57:23 +0700

    User module

--- server.go
+++ server.go
@@ -13,8 +13,10 @@ var (
 	db             *gorm.DB                  = config.SetupDatabaseConnection()
 	userRepository repository.UserRepository = repository.NewUserRepository(db)
 	jwtService     service.JWTService        = service.NewJWTService()
+	userService    service.UserService       = service.NewUserService(userRepository)
 	authService    service.AuthService       = service.NewAuthService(userRepository)
 	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
+	userController controller.UserController = controller.NewUserController(userService, jwtService)
 )
 
 func main() {
@@ -25,6 +27,12 @@ func main() {
 	{
 		authRoutes.POST("/login", authController.Login)
 		authRoutes.POST("/register", authController.Register)
+	}
+
+	userRoutes := r.Group("/api/user")
+	{
+		userRoutes.GET("/profile", userController.Profile)
+		userRoutes.PUT("/profile", userController.Update)
 	}
 
 	r.Run()
